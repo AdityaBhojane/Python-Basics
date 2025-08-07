aa9cd6affe01b6bfd667dbcfb809916330b3fc95
aditya bhojane <bhojaneaditya7@gmail.com>
2025-08-07 19:44:46 +0530

    python baiscs

--- Basics.py
+++ Basics.py
@@ -1,1 +1,39 @@
-# baiscs
+
+# variable declaration
+x = 10
+y = 30
+z = x+y
+# name of the variable and directly put values
+NAME='adi'
+# we can use uppercase latter to make value constant we not going to change 
+# and its just a convection lang doest enforce about it 
+
+# DATA TYPES 
+
+#string
+city_name='pune'
+# f-string its like string interpolation to get best format
+# print(f"city name is {city_name}")
+
+#numbers (int, float)
+math_percentage=88
+overall_percentage=3.22
+
+# print(type(math_percentage))
+
+#Boolean
+is_user_logged_in=True
+is_user_allowed= False
+
+
+#nothing type in python
+# winner=none
+
+# conditionals
+current_age = 20
+if current_age < 18:
+    print("user underage")
+elif current_age == 18:
+    print("user is allowed")
+else:
+    print("user already joined")
